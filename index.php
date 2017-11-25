4e8bd57f6e16776a23fd04416d1008321fe07b34
Vitalii Haievyi <GVitalii1993@gmail.com>
2017-11-25 22:00:33 +0200

    start

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<div class="wrapper">
+	<div class="ship_modeling">
+		
+	</div>
+	<div class="drawing">
+		
+	</div>
+	<div class="Crochet">
+		
+	</div>
+	<div class="coding">
+		
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<div>Test Php</div>
+	<?php 
+	$name = "Name Page";
+	echo $name;
+
+	$sum = 100/4;
+	echo $sum;
+ 	?>
+</div>
+
+
+
+
+
+
+
+<script>document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')</script>
+</body>
+</html>
